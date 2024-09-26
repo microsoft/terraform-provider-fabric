020592370960209ee854a67f706f07440482ddf7
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2024-09-26 06:58:45 -0700

    docs: fix links to guides and enhance auth guides (#17)
    # 📥 Pull Request
    
    ## ❓ What are you trying to address
    
    This pull request includes several updates to documentation to enhance
    user context authentication and fix broken links.
    
    ## ✨ Description of new changes
    
    ### Scope and Path Updates:
    
    * Updated the scope name from `default` to `access` in
    `docs/guides/auth_app_reg_user.md` and
    `templates/guides/auth_app_reg_user.md.tmpl`
    (`api://fabric_terraform_provider`).
    [[1]](diffhunk://#diff-f4296a59cca616742a63b2c76988d84e5a11f52dbdd955ae811ac6915de434f1L46-R46)
    [[2]](diffhunk://#diff-4178a3604d6c95d8729aa4defc2b5bfe976caef501916683761e47797e03fc7bL46-R46)
    * Changed the Azure CLI login scope from
    `api://fabric_terraform_provider/default` to
    `api://fabric_terraform_provider/.default` in
    `docs/guides/auth_app_reg_user.md` and
    `examples/guides/auth_app_reg_user/azcli_login.sh` (`az config set
    core.enable_broker_on_windows=false`).
    [[1]](diffhunk://#diff-f4296a59cca616742a63b2c76988d84e5a11f52dbdd955ae811ac6915de434f1L76-R76)
    [[2]](diffhunk://#diff-c16c65839db8487a1465e8b7e047f17deb53650b577ac837c3efc977dfb919cbL11-R11)
    
    ### Path Correction in Index Files:
    
    * Corrected relative paths in `docs/index.md` and
    `templates/index.md.tmpl` to remove leading `./` for guide links.
    [[1]](diffhunk://#diff-b4d68dc855d0f9476d3f2ee343853bd21bf82ea9960d0cf06661baa244439dd6L64-R73)
    [[2]](diffhunk://#diff-2787aac6ad1510c9157d238b7b366aa4a6abdeba2429c28affad380e0bd3df26L40-R56)
    * Updated the recommendation for using the Azure CLI for authentication
    with user context in `docs/index.md` and `templates/index.md.tmpl`.
    [[1]](diffhunk://#diff-b4d68dc855d0f9476d3f2ee343853bd21bf82ea9960d0cf06661baa244439dd6L109-R109)
    [[2]](diffhunk://#diff-2787aac6ad1510c9157d238b7b366aa4a6abdeba2429c28affad380e0bd3df26L40-R56)

--- examples/guides/auth_app_reg_user/azcli_login.sh
+++ examples/guides/auth_app_reg_user/azcli_login.sh
@@ -8,4 +8,4 @@ az config set core.enable_broker_on_windows=false
 
 # Login to Azure with Entra ID credentials
 # See https://learn.microsoft.com/cli/azure/authenticate-azure-cli for more details.
-az login --allow-no-subscriptions --tenant 00000000-0000-0000-0000-000000000000 --scope api://fabric_terraform_provider/default
+az login --allow-no-subscriptions --tenant 00000000-0000-0000-0000-000000000000 --scope api://fabric_terraform_provider/.default
