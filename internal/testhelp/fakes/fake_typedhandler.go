08aed9523ca648a0896f169ee713978c95282bba
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2024-12-19 01:11:06 +0000

    Working unit tests with generic fabric item logic

--- internal/testhelp/fakes/fake_typedhandler.go
+++ internal/testhelp/fakes/fake_typedhandler.go
@@ -5,6 +5,8 @@ package fakes
 
 import (
 	"context"
+	"reflect"
+	"strings"
 
 	azfake "github.com/Azure/azure-sdk-for-go/sdk/azcore/fake"
 	fabcore "github.com/microsoft/fabric-sdk-go/fabric/core"
@@ -101,9 +103,17 @@ func generateID(parentID, childID string) string {
 func (h *typedHandler[TEntity]) Elements() []TEntity {
 	ret := make([]TEntity, 0)
 
-	for _, element := range h.elements {
-		if element, ok := element.(TEntity); ok {
-			ret = append(ret, element)
+	// if it is a FabricItem, return all the elements as fabric items
+	if h.entityTypeIsFabricItem() {
+		for _, element := range h.elements {
+			item := asFabricItem(element)
+			ret = append(ret, h.getFabricItemAsTEntity(item))
+		}
+	} else {
+		for _, element := range h.elements {
+			if element, ok := element.(TEntity); ok {
+				ret = append(ret, element)
+			}
 		}
 	}
 
@@ -117,6 +127,12 @@ func (h *typedHandler[TEntity]) Delete(id string) {
 	for _, element := range h.elements {
 		if typedElement, ok := element.(TEntity); ok {
 			if h.identifier.GetID(typedElement) != id {
+				newElements = append(newElements, element)
+			}
+		} else if h.entityTypeCanBeConvertedToFabricItem() {
+			// if it wasn't found, try to find it as fabric item
+			item := asFabricItem(element)
+			if !strings.HasSuffix(id, *item.ID) {
 				newElements = append(newElements, element)
 			}
 		} else {
@@ -140,18 +156,56 @@ func (h *typedHandler[TEntity]) Upsert(element TEntity) {
 
 // Get gets an element by ID.
 func (h *typedHandler[TEntity]) Get(id string) TEntity {
-	pointer := h.getPointer(id)
+	// check if it is a FabricItem
+	if !h.entityTypeIsFabricItem() {
+		pointer := h.getPointer(id)
+		if pointer == nil {
+			// if it wasn't found, try to find it as fabric item
+			for _, element := range h.elements {
+				if h.entityTypeCanBeConvertedToFabricItem() {
+					item := asFabricItem(element)
+					if strings.HasSuffix(id, *item.ID) {
+						return h.getFabricItemAsTEntity(item)
+					}
+				}
+			}
 
-	if pointer == nil {
+			// if that didn't work, panic
+			panic("Element not found") // lintignore:R009
+		}
+
+		return *pointer
+	} else {
+		for _, element := range h.elements {
+			item := asFabricItem(element)
+			if strings.HasSuffix(id, *item.ID) {
+				return element.(TEntity)
+			}
+		}
+
 		panic("Element not found") // lintignore:R009
 	}
-
-	return *pointer
 }
 
 // Contains returns true if the element exists.
 func (h *typedHandler[TEntity]) Contains(id string) bool {
-	return h.getPointer(id) != nil
+	found := h.getPointer(id) != nil
+
+	if found {
+		return true
+	}
+
+	// if it wasn't found, try to find it as fabric item
+	if h.entityTypeCanBeConvertedToFabricItem() {
+		for _, element := range h.elements {
+			item := asFabricItem(element)
+			if strings.HasSuffix(id, *item.ID) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 // getPointer gets a pointer to an element by ID.
@@ -165,4 +219,74 @@ func (h *typedHandler[TEntity]) getPointer(id string) *TEntity {
 	}
 
 	return nil
+}
+
+func (h *typedHandler[TEntity]) getFabricItemAsTEntity(item fabcore.Item) TEntity {
+	var entity TEntity
+
+	h.setReflectedStringPropertyValue(&entity, "ID", *item.ID)
+	h.setReflectedStringPropertyValue(&entity, "WorkspaceID", *item.WorkspaceID)
+	h.setReflectedStringPropertyValue(&entity, "DisplayName", *item.DisplayName)
+	h.setReflectedStringPropertyValue(&entity, "Description", *item.Description)
+	h.setReflectedStringPropertyValue(&entity, "Type", string(*item.Type))
+
+	return entity
+}
+
+// asFabricItem converts an element to a fabric item.
+func asFabricItem(element any) fabcore.Item {
+	itemType := fabcore.ItemType(*getReflectedStringPropertyValue(element, "Type"))
+
+	item := fabcore.Item{
+		Type:        &itemType,
+		Description: getReflectedStringPropertyValue(element, "Description"),
+		DisplayName: getReflectedStringPropertyValue(element, "DisplayName"),
+		ID:          getReflectedStringPropertyValue(element, "ID"),
+		WorkspaceID: getReflectedStringPropertyValue(element, "WorkspaceID"),
+	}
+
+	return item
+}
+
+// getReflectedStringPropertyValue gets a string property value from a reflected object.
+func getReflectedStringPropertyValue(element any, propertyName string) *string {
+	reflectedValue := reflect.ValueOf(element)
+	propertyValue := reflectedValue.FieldByName(propertyName)
+
+	str := propertyValue.Elem().String()
+
+	return &str
+}
+
+// setReflectedStringPropertyValue sets a string property value on a reflected object.
+func (h *typedHandler[TEntity]) setReflectedStringPropertyValue(entity *TEntity, propertyName string, value string) {
+	reflectedValue := reflect.ValueOf(entity).Elem()
+	propertyValue := reflectedValue.FieldByName(propertyName)
+
+	// create a new pointer to the type of the property
+	ptr := reflect.New(propertyValue.Type().Elem())
+	ptr.Elem().SetString(value)
+
+	// set the value as a pointer
+	propertyValue.Set(ptr)
+}
+
+func (h *typedHandler[TEntity]) entityTypeIsFabricItem() bool {
+	var entity TEntity
+	return reflect.TypeOf(entity) == reflect.TypeOf(fabcore.Item{})
+}
+
+func (h *typedHandler[TEntity]) entityTypeCanBeConvertedToFabricItem() bool {
+	var entity TEntity
+
+	requiredPropertyNames := []string{"ID", "WorkspaceID", "DisplayName", "Description", "Type"}
+
+	for _, propertyName := range requiredPropertyNames {
+		// check if the property exists
+		if !reflect.ValueOf(entity).FieldByName(propertyName).IsValid() {
+			return false
+		}
+	}
+
+	return true
 }
