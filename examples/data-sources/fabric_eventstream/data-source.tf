262165cdedabd43fbe352a55f27c1625086796e3
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-01-25 12:40:23 +0100

    docs(eventstream): enhance examples with definition support

--- examples/data-sources/fabric_eventstream/data-source.tf
+++ examples/data-sources/fabric_eventstream/data-source.tf
@@ -1,11 +1,32 @@
+# Get item details by name
+data "fabric_eventstream" "example_by_name" {
+  display_name = "example"
+  workspace_id = "00000000-0000-0000-0000-000000000000"
+}
+
+# Get item details by id
 data "fabric_eventstream" "example_by_id" {
   id           = "11111111-1111-1111-1111-111111111111"
   workspace_id = "00000000-0000-0000-0000-000000000000"
 }
 
-data "fabric_eventstream" "example_by_name" {
-  display_name = "example"
-  workspace_id = "00000000-0000-0000-0000-000000000000"
+# Get item details with definition
+# Examples uses `id` but `display_name` can be used as well
+data "fabric_eventstream" "example_definition" {
+  id                = "11111111-1111-1111-1111-111111111111"
+  workspace_id      = "00000000-0000-0000-0000-000000000000"
+  format            = "Default"
+  output_definition = true
+}
+
+# Access the content of the definition with JSONPath expression
+output "example_definition_content_jsonpath" {
+  value = provider::fabric::content_decode(data.fabric_eventstream.example_definition.definition["eventstream.json"].content, ".destinations[0].properties")
+}
+
+# Access the content of the definition as JSON object
+output "example_definition_content_object" {
+  value = provider::fabric::content_decode(data.fabric_eventstream.example_definition.definition["eventstream.json"].content).destinations[0].properties
 }
 
 # This is an invalid data source
