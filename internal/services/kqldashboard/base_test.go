839e7bc0a2cb49064b446d099d0ca6a9361387a4
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-01-30 18:56:19 +0100

    feat(kql_dashboard): onboard new resource/data-source (#218)
    # 📥 Pull Request
    
    close #134
    
    ## ❓ What are you trying to address
    
    Onboard new Resource/Data-Source: `fabric_kql_dashboard`

---
+++ internal/services/kqldashboard/base_test.go
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MPL-2.0
+
+package kqldashboard_test
+
+import (
+	"github.com/microsoft/terraform-provider-fabric/internal/services/kqldashboard"
+)
+
+const (
+	itemTFName  = kqldashboard.ItemTFName
+	itemsTFName = kqldashboard.ItemsTFName
+	itemType    = kqldashboard.ItemType
+)
