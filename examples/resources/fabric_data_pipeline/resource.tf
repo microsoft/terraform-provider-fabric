ae7f718f4046e38a3e2d7ec1cfa4933cd7bb600a
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-03-13 09:07:27 -0700

    feat: add default format to data pipeline resources

--- examples/resources/fabric_data_pipeline/resource.tf
+++ examples/resources/fabric_data_pipeline/resource.tf
@@ -9,6 +9,7 @@ resource "fabric_data_pipeline" "example_definition_bootstrap" {
   display_name              = "example"
   description               = "example with definition bootstrapping"
   workspace_id              = "00000000-0000-0000-0000-000000000000"
+  format                    = "Default"
   definition_update_enabled = false
   definition = {
     "pipeline-content.json" = {
@@ -25,6 +26,7 @@ resource "fabric_data_pipeline" "example_definition_update" {
   display_name = "example"
   description  = "example with definition update when source or tokens changed"
   workspace_id = "00000000-0000-0000-0000-000000000000"
+  format       = "Default"
   definition = {
     "pipeline-content.json" = {
       source = "${local.path}/pipeline-content.json"
