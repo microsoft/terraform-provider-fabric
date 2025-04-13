d9e0c0944d4e601c468991c1c3e4f9ff74080cc1
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-04-13 11:05:37 -0700

    fix(eventhouse): add nolint comments for minimum consumption units variables

--- internal/services/eventhouse/base.go
+++ internal/services/eventhouse/base.go
@@ -35,7 +35,7 @@ var itemDefinitionFormats = []fabricitem.DefinitionFormat{ //nolint:gochecknoglo
 }
 
 var (
-	possibleMinimumConsumptionUnitsValues = []float64{0, 2.25, 4.25, 8.5, 13, 18, 26, 34, 50}
-	minimumConsumptionUnitsMin            = float64(51)
-	minimumConsumptionUnitsMax            = float64(322)
+	possibleMinimumConsumptionUnitsValues = []float64{0, 2.25, 4.25, 8.5, 13, 18, 26, 34, 50} //nolint:gochecknoglobals
+	minimumConsumptionUnitsMin            = float64(51)                                       //nolint:gochecknoglobals
+	minimumConsumptionUnitsMax            = float64(322)                                      //nolint:gochecknoglobals
 )
