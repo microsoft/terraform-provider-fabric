28b604eda36e05656a0e3c7b91d3a6f85494bc45
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-05-14 00:52:30 -0700

    .

--- examples/resources/fabric_connection/resource.tf
+++ examples/resources/fabric_connection/resource.tf
@@ -1,7 +1,40 @@
 # Example 1 - ShareableCloud Connection
 resource "fabric_connection" "example_cloud" {
-  display_name      = "example"
+  display_name      = "example1"
   connectivity_type = "ShareableCloud"
+  privacy_level     = "Organizational"
+  connection_details = {
+    type            = "FTP"
+    creation_method = "FTP.Contents"
+    parameters = [
+      {
+        name  = "server"
+        value = "ftp.example.com"
+      }
+    ]
+  }
+  credential_details = {
+    connection_encryption = "NotEncrypted"
+    credential_type       = "Basic"
+    single_sign_on_type   = "None"
+    skip_test_connection  = false
+    basic_credentials = {
+      username            = "user"
+      password_wo         = "...secret_password1..."
+      password_wo_version = 2
+    }
+  }
+}
+
+# Example 2 - OnPremisesGateway Connection
+output "example_on_premises_gateway" {
+  value = resource.fabric_connection.example_on_premises_gateway
+}
+
+resource "fabric_connection" "example_on_premises_gateway" {
+  gateway_id        = "f0e7cc2c-f62c-4511-a50b-4e54216b92a2"
+  display_name      = "example"
+  connectivity_type = "OnPremisesGateway"
   privacy_level     = "Organizational"
   connection_details = {
     type            = "FTP"
@@ -25,31 +58,3 @@ resource "fabric_connection" "example_cloud" {
     }
   }
 }
-
-# Example 2 - OnPremisesGateway Connection
-# resource "fabric_connection" "example_on_premises_gateway" {
-#   display_name      = "example"
-#   connectivity_type = "OnPremisesGateway"
-#   privacy_level     = "Organizational"
-#   connection_details = {
-#     type            = "FTP"
-#     creation_method = "FTP.Contents"
-#     parameters = [
-#       {
-#         name  = "server"
-#         value = "ftp.example.com"
-#       }
-#     ]
-#   }
-#   credential_details = {
-#     connection_encryption = "NotEncrypted"
-#     credential_type       = "Basic"
-#     single_sign_on_type   = "None"
-#     skip_test_connection  = false
-#     basic_credentials = {
-#       username            = "user"
-#       password_wo         = "...secret_password..."
-#       password_wo_version = 1
-#     }
-#   }
-# }
