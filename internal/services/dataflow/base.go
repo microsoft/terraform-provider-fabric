d889d770ed51bde552828067b661d7aafc9cc77c
Catalina Gavrilovici (QUALITEST) <v-gcatalina@microsoft.com>
2025-05-19 12:41:43 +0000

    add dataflow data source and resource

---
+++ internal/services/dataflow/base.go
@@ -0,0 +1,41 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MPL-2.0
+
+package dataflow
+
+import (
+	fabcore "github.com/microsoft/fabric-sdk-go/fabric/core"
+
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/fabricitem"
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/tftypeinfo"
+)
+
+const (
+	FabricItemType = fabcore.ItemTypeDataflow
+
+	ItemDefinitionEmpty       = `{}`
+	ItemDefinitionPathDocsURL = "https://learn.microsoft.com/fabric/data-factory/data-source-management"
+)
+
+var ItemTypeInfo = tftypeinfo.TFTypeInfo{ //nolint:gochecknoglobals
+	Name:           "Dataflow",
+	Type:           "dataflow",
+	Names:          "Dataflows",
+	Types:          "Dataflows",
+	DocsURL:        "https://learn.microsoft.com/rest/api/fabric/articles/item-management/definitions/dataflow-definition",
+	IsPreview:      false,
+	IsSPNSupported: true,
+}
+
+var itemDefinitionFormats = []fabricitem.DefinitionFormat{
+	{
+		Type:  fabricitem.DefinitionFormatDefault,
+		API:   "",
+		Paths: []string{"queryMetadata.json"},
+	},
+	{
+		Type:  "pq",
+		API:   "",
+		Paths: []string{"mashup.pq"},
+	},
+}
