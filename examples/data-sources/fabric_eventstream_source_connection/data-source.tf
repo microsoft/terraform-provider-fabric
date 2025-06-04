d8ba80657bc3ef953868e58a5c69e9f9029bea89
Badea Marjieh <badeamarjieh@microsoft.com>
2025-06-04 07:33:52 +0000

    add data-source example and docs

--- examples/data-sources/fabric_eventstream_source_connection/data-source.tf
+++ examples/data-sources/fabric_eventstream_source_connection/data-source.tf
@@ -1,5 +1,5 @@
-# Get ephemeral resource details
-ephemeral "fabric_eventstream_source_connection" "example" {
+# Get data-soure details
+data "fabric_eventstream_source_connection" "example" {
   workspace_id   = "00000000-0000-0000-0000-000000000000"
   eventstream_id = "11111111-1111-1111-1111-111111111111"
   source_id      = "22222222-2222-2222-2222-222222222222"
