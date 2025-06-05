a2bb39aea5b38c52227459d250386f1e95c6e879
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-06-04 21:11:17 -0700

    feat(fabricitem): add processing_mode to resources with definition

--- examples/resources/fabric_eventstream/resource.tf
+++ examples/resources/fabric_eventstream/resource.tf
@@ -1,36 +1,65 @@
-# Example 1 - Item without definition
-resource "fabric_eventstream" "example" {
-  display_name = "example"
-  workspace_id = "00000000-0000-0000-0000-000000000000"
-}
+# # Example 1 - Item without definition
+# resource "fabric_eventstream" "example" {
+#   display_name = "example"
+#   workspace_id = "00000000-0000-0000-0000-000000000000"
+# }
 
-# Example 2 - Item with definition bootstrapping only
-resource "fabric_eventstream" "example_definition_bootstrap" {
-  display_name              = "example"
-  description               = "example with definition bootstrapping"
-  workspace_id              = "00000000-0000-0000-0000-000000000000"
-  definition_update_enabled = false
-  format                    = "Default"
-  definition = {
-    "eventstream.json" = {
-      source = "${local.path}/eventstream.json.tmpl"
-    }
-  }
-}
+# # Example 2 - Item with definition bootstrapping only
+# resource "fabric_eventstream" "example_definition_bootstrap" {
+#   display_name              = "example"
+#   description               = "example with definition bootstrapping"
+#   workspace_id              = "00000000-0000-0000-0000-000000000000"
+#   definition_update_enabled = false
+#   format                    = "Default"
+#   definition = {
+#     "eventstream.json" = {
+#       source = "${local.path}/eventstream.json.tmpl"
+#     }
+#   }
+# }
 
-# Example 3 - Item with definition update when source or tokens changed
-resource "fabric_eventstream" "example_definition_update" {
+# # Example 3 - Item with definition update when source or tokens changed
+# resource "fabric_eventstream" "example_definition_update" {
+#   display_name = "example"
+#   description  = "example with definition update when source or tokens changed"
+#   workspace_id = "00000000-0000-0000-0000-000000000000"
+#   format       = "Default"
+#   definition = {
+#     "eventstream.json" = {
+#       source = "${local.path}/eventstream.json.tmpl"
+#       tokens = {
+#         "LakehouseWorkspaceID" = "11111111-1111-1111-1111-111111111111"
+#         "LakehouseID"          = "22222222-2222-2222-2222-222222222222"
+#       }
+#     }
+#   }
+# }
+
+resource "fabric_eventstream" "example_definition_update_parameters" {
   display_name = "example"
-  description  = "example with definition update when source or tokens changed"
+  description  = "example with definition update when source or parameters changed"
   workspace_id = "00000000-0000-0000-0000-000000000000"
   format       = "Default"
   definition = {
     "eventstream.json" = {
-      source = "${local.path}/eventstream.json.tmpl"
-      tokens = {
-        "LakehouseWorkspaceID" = "11111111-1111-1111-1111-111111111111"
-        "LakehouseID"          = "22222222-2222-2222-2222-222222222222"
-      }
+      source          = "${local.path}/eventstream.json"
+      processing_mode = "Parameters"
+      parameters = [
+        {
+          type  = "FindReplace"
+          find  = "00000000-0000-0000-0000-000000000000"
+          value = "22222222-2222-2222-2222-222222222222"
+        },
+        {
+          type  = "KeyValueReplace"
+          find  = "$.destinations[?(@.name=='Lakehouse')].properties.itemId"
+          value = "33333333-3333-3333-3333-333333333333"
+        }
+      ]
     }
   }
+}
+
+output "example" {
+  value = fabric_eventstream.example_definition_update_parameters
 }
