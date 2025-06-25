c0d4f4e4e2ef73c72214527d100be1c508eac498
Catalina Gavrilovici <61465343+catalina-gav@users.noreply.github.com>
2025-06-25 18:07:19 +0300

    feat(onelake_shortcut): onboard new data-source and resource (#424)
    # 📥 Pull Request
    
    ## ❓ What are you trying to address
    close https://github.com/microsoft/terraform-provider-fabric/issues/422
    close https://github.com/microsoft/terraform-provider-fabric/issues/421
    
    Onboard new onelake shortcut resource/data-source
    
    ---------
    
    Co-authored-by: Catalina Gavrilovici (QUALITEST) <v-gcatalina@microsoft.com>

---
+++ internal/services/shortcut/resource_shortcut.go
@@ -0,0 +1,318 @@
+// // Copyright (c) Microsoft Corporation
+// // SPDX-License-Identifier: MPL-2.0
+
+package shortcut
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework-validators/resourcevalidator"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	fabcore "github.com/microsoft/fabric-sdk-go/fabric/core"
+
+	"github.com/microsoft/terraform-provider-fabric/internal/common"
+	"github.com/microsoft/terraform-provider-fabric/internal/framework/customtypes"
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/tftypeinfo"
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/utils"
+	pconfig "github.com/microsoft/terraform-provider-fabric/internal/provider/config"
+)
+
+var (
+	_ resource.ResourceWithConfigure        = (*resourceShortcut)(nil)
+	_ resource.ResourceWithImportState      = (*resourceShortcut)(nil)
+	_ resource.ResourceWithConfigValidators = (*resourceShortcut)(nil)
+)
+
+type resourceShortcut struct {
+	pConfigData *pconfig.ProviderData
+	client      *fabcore.OneLakeShortcutsClient
+	TypeInfo    tftypeinfo.TFTypeInfo
+}
+
+func NewResourceShortcut() resource.Resource {
+	return &resourceShortcut{
+		TypeInfo: ItemTypeInfo,
+	}
+}
+
+func (r *resourceShortcut) Metadata(_ context.Context, _ resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = r.TypeInfo.FullTypeName(false)
+}
+
+func (r *resourceShortcut) Schema(ctx context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = itemSchema(false).GetResource(ctx)
+}
+
+func (r *resourceShortcut) ConfigValidators(_ context.Context) []resource.ConfigValidator {
+	return []resource.ConfigValidator{
+		resourcevalidator.ExactlyOneOf(
+			path.MatchRoot("target").AtName("onelake"),
+			path.MatchRoot("target").AtName("adls_gen2"),
+			path.MatchRoot("target").AtName("amazon_s3"),
+			path.MatchRoot("target").AtName("google_cloud_storage"),
+			path.MatchRoot("target").AtName("s3_compatible"),
+			path.MatchRoot("target").AtName("dataverse"),
+		),
+	}
+}
+
+func (r *resourceShortcut) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	pConfigData, ok := req.ProviderData.(*pconfig.ProviderData)
+	if !ok {
+		resp.Diagnostics.AddError(
+			common.ErrorResourceConfigType,
+			fmt.Sprintf(common.ErrorFabricClientType, req.ProviderData),
+		)
+
+		return
+	}
+
+	r.pConfigData = pConfigData
+	r.client = fabcore.NewClientFactoryWithClient(*pConfigData.FabricClient).NewOneLakeShortcutsClient()
+}
+
+func (r *resourceShortcut) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	tflog.Debug(ctx, "CREATE", map[string]any{
+		"action": "start",
+	})
+
+	var plan, state resourceShortcutModel
+
+	if resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	timeout, diags := plan.Timeouts.Create(ctx, r.pConfigData.Timeout)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	state.Timeouts = plan.Timeouts
+
+	var reqCreate requestCreateShortcut
+
+	if resp.Diagnostics.Append(reqCreate.set(ctx, plan)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	respCreate, err := r.client.CreateShortcut(ctx, plan.WorkspaceID.ValueString(), plan.ItemID.ValueString(), reqCreate.CreateShortcutRequest, nil)
+	if resp.Diagnostics.Append(utils.GetDiagsFromError(ctx, err, utils.OperationCreate, nil)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	if resp.Diagnostics.Append(state.set(ctx, plan.WorkspaceID.ValueString(), plan.ItemID.ValueString(), respCreate.Shortcut)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+
+	tflog.Debug(ctx, "CREATE", map[string]any{
+		"action": "end",
+	})
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *resourceShortcut) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	tflog.Debug(ctx, "UPDATE", map[string]any{"action": "start"})
+
+	var plan, state resourceShortcutModel
+	if resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	timeout, diags := plan.Timeouts.Update(ctx, r.pConfigData.Timeout)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	state.Timeouts = plan.Timeouts
+
+	var reqCreate requestCreateShortcut
+
+	if resp.Diagnostics.Append(reqCreate.set(ctx, plan)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	overwriteOnlyPolicy := fabcore.ShortcutConflictPolicyOverwriteOnly
+	options := fabcore.OneLakeShortcutsClientCreateShortcutOptions{
+		ShortcutConflictPolicy: &overwriteOnlyPolicy,
+	}
+
+	respCreate, err := r.client.CreateShortcut(ctx, plan.WorkspaceID.ValueString(), plan.ItemID.ValueString(), reqCreate.CreateShortcutRequest, &options)
+	if resp.Diagnostics.Append(utils.GetDiagsFromError(ctx, err, utils.OperationUpdate, nil)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	if resp.Diagnostics.Append(state.set(ctx, plan.WorkspaceID.ValueString(), plan.ItemID.ValueString(), respCreate.Shortcut)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+
+	tflog.Debug(ctx, "UPDATE", map[string]any{"action": "end"})
+}
+
+func (r *resourceShortcut) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	tflog.Debug(ctx, "READ", map[string]any{
+		"action": "start",
+	})
+
+	var state resourceShortcutModel
+
+	if resp.Diagnostics.Append(req.State.Get(ctx, &state)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	timeout, diags := state.Timeouts.Read(ctx, r.pConfigData.Timeout)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	diags = r.get(ctx, &state)
+	if utils.IsErrNotFound(state.ID.ValueString(), &diags, fabcore.ErrCommon.EntityNotFound) {
+		resp.State.RemoveResource(ctx)
+
+		resp.Diagnostics.Append(diags...)
+
+		return
+	}
+
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+
+	tflog.Debug(ctx, "READ", map[string]any{
+		"action": "end",
+	})
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *resourceShortcut) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	tflog.Debug(ctx, "DELETE", map[string]any{
+		"action": "start",
+	})
+
+	var state resourceShortcutModel
+
+	if resp.Diagnostics.Append(req.State.Get(ctx, &state)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	timeout, diags := state.Timeouts.Delete(ctx, r.pConfigData.Timeout)
+	if resp.Diagnostics.Append(diags...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	_, err := r.client.DeleteShortcut(ctx, state.WorkspaceID.ValueString(), state.ItemID.ValueString(), state.Path.ValueString(), state.Name.ValueString(), nil)
+	if resp.Diagnostics.Append(utils.GetDiagsFromError(ctx, err, utils.OperationDelete, nil)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.State.RemoveResource(ctx)
+
+	tflog.Debug(ctx, "DELETE", map[string]any{
+		"action": "end",
+	})
+}
+
+func (r *resourceShortcut) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	tflog.Debug(ctx, "IMPORT", map[string]any{
+		"action": "start",
+	})
+	tflog.Trace(ctx, "IMPORT", map[string]any{
+		"id": req.ID,
+	})
+
+	parts := strings.Split(req.ID, "/")
+	if len(parts) != 4 {
+		resp.Diagnostics.AddError(
+			common.ErrorImportIdentifierHeader,
+			fmt.Sprintf(common.ErrorImportIdentifierDetails, "WorkspaceID/ItemID/Path/Name"),
+		)
+
+		return
+	}
+
+	workspaceID, itemID, shortcutPath, name := parts[0], parts[1], parts[2], parts[3]
+
+	uuidWorkspaceID, diags := customtypes.NewUUIDValueMust(workspaceID)
+	resp.Diagnostics.Append(diags...)
+
+	uuitemID, diags := customtypes.NewUUIDValueMust(itemID)
+	resp.Diagnostics.Append(diags...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var timeout timeouts.Value
+	if resp.Diagnostics.Append(resp.State.GetAttribute(ctx, path.Root("timeouts"), &timeout)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	state := resourceShortcutModel{
+		baseShortcutModel: baseShortcutModel{
+			ItemID:      uuitemID,
+			WorkspaceID: uuidWorkspaceID,
+			Name:        types.StringValue(name),
+			Path:        types.StringValue(shortcutPath),
+		},
+		Timeouts: timeout,
+	}
+
+	if resp.Diagnostics.Append(r.get(ctx, &state)...); resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
+
+	tflog.Debug(ctx, "IMPORT", map[string]any{
+		"action": "end",
+	})
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *resourceShortcut) get(ctx context.Context, model *resourceShortcutModel) diag.Diagnostics {
+	respGet, err := r.client.GetShortcut(ctx, model.WorkspaceID.ValueString(), model.ItemID.ValueString(), model.Path.ValueString(), model.Name.ValueString(), nil)
+	if diags := utils.GetDiagsFromError(ctx, err, utils.OperationRead, fabcore.ErrCommon.EntityNotFound); diags.HasError() {
+		return diags
+	}
+
+	model.set(ctx, model.WorkspaceID.ValueString(), model.ItemID.ValueString(), respGet.Shortcut)
+
+	return nil
+}
