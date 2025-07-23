5bed6dbaf4b4b96c7d09dd5f92201a07e364c937
Mihai Dabuleanu (QUALITEST) <v-mdabuleanu@microsoft.com>
2025-07-23 21:36:47 +0000

    initial script commit

---
+++ examples/resources/fabric_variable_library/outputs.tf
@@ -0,0 +1,3 @@
+output "example" {
+  value = fabric_variable_library.example
+}
---
+++ internal/services/variablelibrary/base.go
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MPL-2.0
+
+package variablelibrary
+
+import (
+	fabcore "github.com/microsoft/fabric-sdk-go/fabric/core"
+
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/fabricitem"
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/tftypeinfo"
+)
+
+const (
+	FabricItemType = fabcore.ItemTypeVariableLibrary
+
+	ItemDefinitionEmpty       = `{}`
+	ItemDefinitionPathDocsURL = "https://learn.microsoft.com/rest/api/fabric/articles/item-management/definitions/variable-library-definition"
+)
+
+var ItemTypeInfo = tftypeinfo.TFTypeInfo{ //nolint:gochecknoglobals
+	Name:           "Variable Library",
+	Type:           "variable_library",
+	Names:          "Variable Libraries",
+	Types:          "variable_libraries",
+	DocsURL:        "https://learn.microsoft.com/fabric/cicd/variable-library/get-started-variable-libraries",
+	IsPreview:      false,
+	IsSPNSupported: true,
+}
+
+var itemDefinitionFormats = []fabricitem.DefinitionFormat{
+	{
+		Type:  fabricitem.DefinitionFormatDefault,
+		API:   "",
+		Paths: []string{"Variables.json"},
+	},
+}
