95bf06954f0319127d6cfbe485ecdd9c0fba71f8
Mihai Dabuleanu (QUALITEST) <v-mdabuleanu@microsoft.com>
2025-07-27 23:49:59 +0000

    feat(variable_library): Onboard new Variable Library Data Source and Resource

---
+++ internal/services/variablelibrary/resource_variable_library.go
@@ -0,0 +1,33 @@
+// Copyright (c) Microsoft Corporation
+// SPDX-License-Identifier: MPL-2.0
+
+package variablelibrary
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework-validators/mapvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/fabricitem"
+)
+
+func NewResourceVariableLibrary() resource.Resource {
+	config := fabricitem.ResourceFabricItemDefinition{
+		TypeInfo:              ItemTypeInfo,
+		FabricItemType:        FabricItemType,
+		NameRenameAllowed:     true,
+		DisplayNameMaxLength:  123,
+		DescriptionMaxLength:  256,
+		DefinitionPathDocsURL: ItemDefinitionPathDocsURL,
+		DefinitionPathKeysValidator: []validator.Map{
+			mapvalidator.SizeAtMost(len(itemDefinitionFormats)),
+			mapvalidator.KeysAre(stringvalidator.OneOf(fabricitem.GetDefinitionFormatPaths(itemDefinitionFormats, fabricitem.DefinitionFormatDefault)...)),
+		},
+		DefinitionRequired: false,
+		DefinitionEmpty:    ItemDefinitionEmpty,
+		DefinitionFormats:  itemDefinitionFormats,
+	}
+
+	return fabricitem.NewResourceFabricItemDefinition(config)
+}
