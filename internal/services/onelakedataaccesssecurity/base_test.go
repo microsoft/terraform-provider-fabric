4939ed48c4261ca58a14c880123ecd7275a3ab77
Or Bauberg <148867472+OrBaubergMicrosoft@users.noreply.github.com>
2025-11-26 18:01:15 +0200

    revert(onelake_data_access_security): Revert OneLake Data Access Security Data Source and Resource (#747)
    Reverts microsoft/terraform-provider-fabric#546

--- internal/services/onelakedataaccesssecurity/base_test.go
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation
-// SPDX-License-Identifier: MPL-2.0
-
-package onelakedataaccesssecurity_test
-
-import "github.com/microsoft/terraform-provider-fabric/internal/services/onelakedataaccesssecurity"
-
-var itemTypeInfo = onelakedataaccesssecurity.ItemTypeInfo
