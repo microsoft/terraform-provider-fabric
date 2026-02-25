ce397304d349a43879513967cfc15d11cc8c5833
Catalina Gavrilovici (QUALITEST) <v-gcatalina@microsoft.com>
2026-02-25 14:48:22 +0000

    keep resource in preview, add unit tests

--- internal/services/sparkenvsettings/resource_spark_environment_settings_test.go
+++ internal/services/sparkenvsettings/resource_spark_environment_settings_test.go
@@ -4,16 +4,221 @@
 package sparkenvsettings_test
 
 import (
+	"regexp"
 	"testing"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	at "github.com/dcarbone/terraform-plugin-framework-utils/v3/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	fabenvironment "github.com/microsoft/fabric-sdk-go/fabric/environment"
 
 	"github.com/microsoft/terraform-provider-fabric/internal/common"
+	"github.com/microsoft/terraform-provider-fabric/internal/framework/customtypes"
 	"github.com/microsoft/terraform-provider-fabric/internal/testhelp"
+	"github.com/microsoft/terraform-provider-fabric/internal/testhelp/fakes"
 )
 
 var testResourceItemFQN, testResourceItemHeader = testhelp.TFResource(common.ProviderTypeName, itemTypeInfo.Type, "test")
+
+func TestUnit_SparkEnvSettingsResource_Attributes(t *testing.T) {
+	resource.ParallelTest(t, testhelp.NewTestUnitCase(t, &testResourceItemFQN, fakes.FakeServer.ServerFactory, nil, []resource.TestStep{
+		// error - no attributes
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{},
+			),
+			ExpectError: regexp.MustCompile(`Missing required argument`),
+		},
+		// error - workspace_id - invalid UUID
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       "invalid uuid",
+					"environment_id":     testhelp.RandomUUID(),
+					"publication_status": "Published",
+				},
+			),
+			ExpectError: regexp.MustCompile(customtypes.UUIDTypeErrorInvalidStringHeader),
+		},
+		// error - environment_id - invalid UUID
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       testhelp.RandomUUID(),
+					"environment_id":     "invalid uuid",
+					"publication_status": "Published",
+				},
+			),
+			ExpectError: regexp.MustCompile(customtypes.UUIDTypeErrorInvalidStringHeader),
+		},
+		// error - unexpected attribute
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       testhelp.RandomUUID(),
+					"environment_id":     testhelp.RandomUUID(),
+					"publication_status": "Published",
+					"unexpected_attr":    "test",
+				},
+			),
+			ExpectError: regexp.MustCompile(`An argument named "unexpected_attr" is not expected here`),
+		},
+		// error - missing workspace_id
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"environment_id":     testhelp.RandomUUID(),
+					"publication_status": "Published",
+				},
+			),
+			ExpectError: regexp.MustCompile(`The argument "workspace_id" is required, but no definition was found.`),
+		},
+		// error - missing environment_id
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       testhelp.RandomUUID(),
+					"publication_status": "Published",
+				},
+			),
+			ExpectError: regexp.MustCompile(`The argument "environment_id" is required, but no definition was found.`),
+		},
+		// error - missing publication_status
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":   testhelp.RandomUUID(),
+					"environment_id": testhelp.RandomUUID(),
+				},
+			),
+			ExpectError: regexp.MustCompile(`The argument "publication_status" is required, but no definition was found.`),
+		},
+		// error - invalid key format in spark_properties
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       testhelp.RandomUUID(),
+					"environment_id":     testhelp.RandomUUID(),
+					"publication_status": "Published",
+					"spark_properties": []map[string]any{
+						{
+							"key":   "spark test",
+							"value": "12",
+						},
+					},
+				},
+			),
+			ExpectError: regexp.MustCompile(`Invalid Attribute Value Match`),
+		},
+	}))
+}
+
+func TestUnit_SparkEnvironmentSettingsResource_CRUD(t *testing.T) {
+	workspaceID := testhelp.RandomUUID()
+	environmentID := testhelp.RandomUUID()
+
+	stagingSparkCompute := NewRandomSparkCompute()
+	stagingSparkCompute.SparkProperties = []fabenvironment.SparkProperty{
+		{
+			Key:   to.Ptr("spark.acls.enable"),
+			Value: to.Ptr("false"),
+		},
+		{
+			Key:   to.Ptr("spark.acls.groups"),
+			Value: to.Ptr("test"),
+		},
+	}
+
+	fakeTestUpsertSparkComputeStaging(environmentID, stagingSparkCompute)
+
+	fakes.FakeServer.ServerFactory.Environment.StagingServer.GetSparkCompute = fakeGetStagingSparkComputeFunc()
+	fakes.FakeServer.ServerFactory.Environment.StagingServer.UpdateSparkCompute = fakeUpdateStagingSparkComputeFunc()
+	fakes.FakeServer.ServerFactory.Environment.PublishedServer.GetSparkCompute = fakeGetPublishedSparkComputeFunc()
+	fakes.FakeServer.ServerFactory.Environment.ItemsServer.BeginPublishEnvironment = fakeBeginPublishEnvironmentFunc()
+
+	resource.ParallelTest(t, testhelp.NewTestUnitCase(t, &testResourceItemFQN, fakes.FakeServer.ServerFactory, nil, []resource.TestStep{
+		// Create and Read
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       workspaceID,
+					"environment_id":     environmentID,
+					"publication_status": "Published",
+					"driver_cores":       4,
+				},
+			),
+			Check: resource.ComposeAggregateTestCheckFunc(
+				resource.TestCheckResourceAttr(testResourceItemFQN, "id", environmentID),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "pool.name", "Starter Pool"),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "driver_cores", "4"),
+			),
+		},
+		// Update and Read
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       workspaceID,
+					"environment_id":     environmentID,
+					"publication_status": "Published",
+					"driver_cores":       8,
+					"spark_properties": []map[string]any{
+						{
+							"key":   "spark.acls.enable",
+							"value": "true",
+						},
+					},
+				},
+			),
+			Check: resource.ComposeAggregateTestCheckFunc(
+				resource.TestCheckResourceAttr(testResourceItemFQN, "id", environmentID),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "pool.name", "Starter Pool"),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "driver_cores", "8"),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "spark_properties.0.key", "spark.acls.enable"),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "spark_properties.0.value", "true"),
+				resource.TestCheckNoResourceAttr(testResourceItemFQN, "spark_properties.1.key"),
+			),
+		},
+		// Update and Read - remove spark properties
+		{
+			ResourceName: testResourceItemFQN,
+			Config: at.CompileConfig(
+				testResourceItemHeader,
+				map[string]any{
+					"workspace_id":       workspaceID,
+					"environment_id":     environmentID,
+					"publication_status": "Published",
+					"driver_cores":       8,
+				},
+			),
+			Check: resource.ComposeAggregateTestCheckFunc(
+				resource.TestCheckResourceAttr(testResourceItemFQN, "id", environmentID),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "pool.name", "Starter Pool"),
+				resource.TestCheckResourceAttr(testResourceItemFQN, "driver_cores", "8"),
+				resource.TestCheckNoResourceAttr(testResourceItemFQN, "spark_properties"),
+			),
+		},
+	}))
+}
 
 func TestAcc_SparkEnvironmentSettingsResource_CRUD(t *testing.T) {
 	capacity := testhelp.WellKnown()["Capacity"].(map[string]any)
@@ -86,7 +291,7 @@ func TestAcc_SparkEnvironmentSettingsSparkPropertiesResource_CRUD(t *testing.T) 
 					map[string]any{
 						"workspace_id":       testhelp.RefByFQN(workspaceResourceFQN, "id"),
 						"environment_id":     testhelp.RefByFQN(environmentResourceFQN, "id"),
-						"publication_status": "Published",
+						"publication_status": "Staging",
 						"driver_cores":       8,
 						"spark_properties": []map[string]any{
 							{
@@ -119,7 +324,7 @@ func TestAcc_SparkEnvironmentSettingsSparkPropertiesResource_CRUD(t *testing.T) 
 					map[string]any{
 						"workspace_id":       testhelp.RefByFQN(workspaceResourceFQN, "id"),
 						"environment_id":     testhelp.RefByFQN(environmentResourceFQN, "id"),
-						"publication_status": "Published",
+						"publication_status": "Staging",
 						"driver_cores":       8,
 						"spark_properties": []map[string]any{
 							{
@@ -137,7 +342,7 @@ func TestAcc_SparkEnvironmentSettingsSparkPropertiesResource_CRUD(t *testing.T) 
 				resource.TestCheckResourceAttr(testResourceItemFQN, "spark_properties.#", "1"),
 			),
 		},
-		// Update and Read (remove Spark properties)
+		// Create and Read (remove Spark properties)
 		{
 			ResourceName: testResourceItemFQN,
 			Config: at.JoinConfigs(
