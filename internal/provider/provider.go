2cb487e7dd44b797c4955274ae782ebfe4ce202a
Dabuleanu Catalin <32688475+CatalinDabuleanu@users.noreply.github.com>
2026-03-02 10:49:19 +0200

    feat(externaldatashare): onboard new resource and data sourc… (#600)
    …e external data shares
    
    # 📥 Pull Request
    
    ## ❓ What are you trying to address
    
    Onboard new data source and resource for External Data Shares Provider
    
    ---------
    
    Co-authored-by: Badea Marjieh <127952241+badeamarjieh@users.noreply.github.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -58,6 +58,7 @@ import (
 	"github.com/microsoft/terraform-provider-fabric/internal/services/eventhouse"
 	"github.com/microsoft/terraform-provider-fabric/internal/services/eventstream"
 	"github.com/microsoft/terraform-provider-fabric/internal/services/eventstreamsourceconnection"
+	"github.com/microsoft/terraform-provider-fabric/internal/services/externaldatashare"
 	"github.com/microsoft/terraform-provider-fabric/internal/services/fabricmap"
 	"github.com/microsoft/terraform-provider-fabric/internal/services/folder"
 	"github.com/microsoft/terraform-provider-fabric/internal/services/gateway"
@@ -456,6 +457,7 @@ func (p *FabricProvider) Resources(ctx context.Context) []func() resource.Resour
 		func() resource.Resource { return environment.NewResourceEnvironment(ctx) },
 		func() resource.Resource { return eventhouse.NewResourceEventhouse(ctx) },
 		eventstream.NewResourceEventstream,
+		externaldatashare.NewResourceExternalDataShares,
 		fabricmap.NewResourceMap,
 		folder.NewResourceFolder,
 		gateway.NewResourceGateway,
@@ -528,6 +530,8 @@ func (p *FabricProvider) DataSources(ctx context.Context) []func() datasource.Da
 		eventstream.NewDataSourceEventstream,
 		eventstream.NewDataSourceEventstreams,
 		eventstreamsourceconnection.NewDataSourceEventstreamSourceConnection,
+		externaldatashare.NewDataSourceExternalDataShare,
+		externaldatashare.NewDataSourceExternalDataShares,
 		fabricmap.NewDataSourceMap,
 		fabricmap.NewDataSourceMaps,
 		folder.NewDataSourceFolder,
---
+++ internal/services/externaldatashare/base_test.go
@@ -0,0 +1,31 @@
+// Copyright Microsoft Corporation 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package externaldatashare_test
+
+import (
+	"testing"
+
+	at "github.com/dcarbone/terraform-plugin-framework-utils/v3/acctest"
+
+	"github.com/microsoft/terraform-provider-fabric/internal/services/externaldatashare"
+	"github.com/microsoft/terraform-provider-fabric/internal/testhelp"
+)
+
+var itemTypeInfo = externaldatashare.ItemTypeInfo
+
+func lakehouseResource(t *testing.T, workspaceID string) (resourceHCL, resourceFQN string) {
+	t.Helper()
+
+	resourceHCL = at.CompileConfig(
+		at.ResourceHeader(testhelp.TypeName("fabric", "lakehouse"), "test"),
+		map[string]any{
+			"display_name": testhelp.RandomName(),
+			"workspace_id": workspaceID,
+		},
+	)
+
+	resourceFQN = testhelp.ResourceFQN("fabric", "lakehouse", "test")
+
+	return resourceHCL, resourceFQN
+}
