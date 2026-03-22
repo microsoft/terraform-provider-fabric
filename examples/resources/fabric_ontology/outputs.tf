b58b20678a9e2f765ff0463cbb32c4f169b7e220
Mihai Dabuleanu (QUALITEST) <v-mdabuleanu@microsoft.com>
2026-03-22 22:47:31 +0000

    added examples + fix copilot issues

---
+++ examples/resources/fabric_ontology/outputs.tf
@@ -0,0 +1,7 @@
+output "example" {
+  value = fabric_ontology.example
+}
+
+output "example_definition_bootstrap" {
+  value = fabric_ontology.example_definition_bootstrap
+}
--- internal/services/ontology/resource_ontology.go
+++ internal/services/ontology/resource_ontology.go
@@ -5,10 +5,15 @@ package ontology
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/mapvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 
+	fwvalidators "github.com/microsoft/terraform-provider-fabric/internal/framework/validators"
 	"github.com/microsoft/terraform-provider-fabric/internal/pkg/fabricitem"
+	"github.com/microsoft/terraform-provider-fabric/internal/pkg/utils"
 )
 
 func NewResourceOntology() resource.Resource {
@@ -20,7 +25,18 @@ func NewResourceOntology() resource.Resource {
 		DescriptionMaxLength:  256,
 		DefinitionPathDocsURL: ItemDefinitionPathDocsURL,
 		DefinitionPathKeysValidator: []validator.Map{
-			mapvalidator.KeysAre(fabricitem.DefinitionPathKeysValidator(itemDefinitionFormats)...),
+			mapvalidator.KeysAre(
+				fwvalidators.PatternsIfAttributeIsOneOf(
+					path.MatchRoot("format"),
+					[]attr.Value{types.StringValue(fabricitem.DefinitionFormatDefault)},
+					fabricitem.GetDefinitionFormatPaths(itemDefinitionFormats, fabricitem.DefinitionFormatDefault),
+					"Definition path must match one of the following: "+utils.ConvertStringSlicesToString(
+						fabricitem.GetDefinitionFormatPaths(itemDefinitionFormats, fabricitem.DefinitionFormatDefault),
+						true,
+						false,
+					),
+				),
+			),
 		},
 		DefinitionRequired: false,
 		DefinitionEmpty:    ItemDefinitionEmpty,
