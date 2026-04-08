822f7768af784a0a1d82ae54654517de80153729
Catalina Gavrilovici (QUALITEST) <v-gcatalina@microsoft.com>
2026-04-08 09:38:01 +0000

    add wellknown operations agent creation

--- tools/scripts/Set-WellKnown.ps1
+++ tools/scripts/Set-WellKnown.ps1
@@ -303,6 +303,9 @@ function Set-FabricItem {
     'Ontology' {
       $itemEndpoint = 'ontologies'
     }
+    'OperationsAgent' {
+      $itemEndpoint = 'operationsAgents'
+    }
     'Reflex' {
       $itemEndpoint = 'reflexes'
     }
@@ -1208,6 +1211,7 @@ $itemNaming = @{
   'MountedDataFactory'              = 'mdf'
   'Notebook'                        = 'nb'
   'Ontology'                        = 'ont'
+  'OperationsAgent'                 = 'oa'
   'Shortcut'                        = 'srt'
   'PaginatedReport'                 = 'prpt'
   'Reflex'                          = 'rx'
@@ -1340,7 +1344,7 @@ $wellKnown['WorkspaceDS'] = @{
 Set-FabricWorkspaceRoleAssignment -WorkspaceId $workspace.id -SG $SPNS_SG
 
 # Define an array of item types to create
-$itemTypes = @('ApacheAirflowJob', 'CopyJob', 'CosmosDB', 'Dataflow', 'DataPipeline', 'DigitalTwinBuilder', 'Environment', 'Eventhouse', 'GraphQLApi', 'KQLDashboard', 'KQLQueryset', 'Lakehouse', 'Map', 'MLExperiment', 'MLModel', 'Notebook', 'Ontology', 'Reflex', 'SparkJobDefinition', 'SQLDatabase', 'VariableLibrary', 'Warehouse')
+$itemTypes = @('ApacheAirflowJob', 'CopyJob', 'CosmosDB', 'Dataflow', 'DataPipeline', 'DigitalTwinBuilder', 'Environment', 'Eventhouse', 'GraphQLApi', 'KQLDashboard', 'KQLQueryset', 'Lakehouse', 'Map', 'MLExperiment', 'MLModel', 'Notebook', 'Ontology', 'OperationsAgent', 'Reflex', 'SparkJobDefinition', 'SQLDatabase', 'VariableLibrary', 'Warehouse')
 
 # Loop through each item type and create if not exists
 foreach ($itemType in $itemTypes) {
