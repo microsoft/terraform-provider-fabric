da7de1063f201f7dadf2accc99ea2398dc84211e
Mihai Dabuleanu (QUALITEST) <v-mdabuleanu@microsoft.com>
2026-04-20 08:11:59 +0000

    added example with definition in data source

--- examples/data-sources/fabric_data_agent/data-source.tf
+++ examples/data-sources/fabric_data_agent/data-source.tf
@@ -8,6 +8,23 @@ data "fabric_data_agent" "example_by_name" {
   workspace_id = "00000000-0000-0000-0000-000000000000"
 }
 
+data "fabric_data_agent" "example_definition" {
+  id                = "11111111-1111-1111-1111-111111111111"
+  workspace_id      = "00000000-0000-0000-0000-000000000000"
+  format            = "Default"
+  output_definition = true
+}
+
+# Access the content of the definition with JSONPath expression
+output "example_definition_content_jsonpath" {
+  value = provider::fabric::content_decode(data.fabric_data_agent.example_definition.definition["data_agent.json"].content, "")
+}
+
+# Access the content of the definition as JSON object
+output "example_definition_content_object" {
+  value = provider::fabric::content_decode(data.fabric_data_agent.example_definition.definition["data_agent.json"].content)
+}
+
 # This is an invalid data source
 # Do not specify `id` and `display_name` in the same data source block
 # data "fabric_data_agent" "example" {
