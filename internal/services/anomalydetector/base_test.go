4823818aa9b44eaa600421b7f0569886de555bc9
Catalina Gavrilovici <61465343+catalina-gav@users.noreply.github.com>
2026-07-06 12:09:57 +0300

    feat(anomaly_detector): onboard data source and resource (#1014)
    # 📥 Pull Request
    
    ## ❓ What are you trying to address
    Close #811 
    Close #812
    
    ---------
    
    Co-authored-by: Catalina Gavrilovici (QUALITEST) <v-gcatalina@microsoft.com>
    Co-authored-by: Or Bauberg <148867472+OrBaubergMicrosoft@users.noreply.github.com>

---
+++ internal/services/anomalydetector/base_test.go
@@ -0,0 +1,12 @@
+// Copyright Microsoft Corporation 2026
+// SPDX-License-Identifier: MPL-2.0
+
+package anomalydetector_test
+
+import (
+	"github.com/microsoft/terraform-provider-fabric/internal/services/anomalydetector"
+)
+
+var itemTypeInfo = anomalydetector.ItemTypeInfo
+
+const fabricItemType = anomalydetector.FabricItemType
